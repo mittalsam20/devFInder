d3c00108ed9af43ebb01b66d5b5c0de39ff90f11
samaksh mittal <sam.mittal44@gmail.com>
2024-08-13 19:31:17 +0530

    feat: use vercel serverless postgresql instead of docker local postgresql | connect webapp with serverless psql

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  driver: "pg",
+  dialect: "postgresql",
+  out: "./drizzle",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    // url: process.env.DATABASE_URL!,
+    url: "postgres://default:3uXWBdOl7rDC@ep-floral-paper-a18ujfii.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require",
   },
   verbose: true,
   strict: true,
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,6 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL!;
-
-// Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false });
-const db = drizzle(client, { schema });
-
+const db = drizzle(sql, { schema });
 export { db };
