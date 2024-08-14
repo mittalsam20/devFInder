099bda689b1351dd0171828d1dbfccb70c725f32
samaksh mittal <sam.mittal44@gmail.com>
2024-08-14 15:22:41 +0530

    fix: add / in protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["create-room", "/your-rooms", "/browse", "/edit-room"],
+  matcher: ["/create-room", "/your-rooms", "/browse", "/edit-room"],
 };
